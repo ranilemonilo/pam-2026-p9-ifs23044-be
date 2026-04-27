e0fb8bc9ff0fde54ea0a51cee40165d9812e084b
ranilemonilo <207569037+ranilemonilo@users.noreply.github.com>
2026-04-27 20:32:08 +0700

    init

--- app/utils/config.py
+++ app/utils/config.py
@@ -18,3 +18,5 @@ class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-key")
     ADMIN_USERNAME = os.getenv("ADMIN_USERNAME", "admin")
     ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD", "admin123")
+
+    APP_PORT = int(os.getenv("APP_PORT", 5000))
